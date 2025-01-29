2546890b9d9a16c6f6523551757f38c71367f521
yusuke <yusuke.iritani0909@gmail.com>
2025-01-30 01:00:39 +0900

    Add logging to Ping endpoint

--- apps/backend/src/cmd/server/ping.go
+++ apps/backend/src/cmd/server/ping.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"fmt"
 
 	pb "rockstart/src/_generated/proto/v1"
 	sqlc "rockstart/src/_generated/sqlc"
@@ -13,5 +14,6 @@ type PingServer struct {
 }
 
 func (s *PingServer) Ping(ctx context.Context, req *pb.PingRequest) (*pb.PingResponse, error) {
+	fmt.Println("Ping request received")
 	return &pb.PingResponse{Message: "Pong"}, nil
 }
