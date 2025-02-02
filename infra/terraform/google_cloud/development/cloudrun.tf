cd4ee0f6bacfc87a68f1533941767ad4166d0943
yusuke <yusuke.iritani0909@gmail.com>
2025-02-02 21:26:34 +0900

    Update Cloud Run backend service image configuration
    - Uncomment the actual backend image path
    - Remove placeholder hello image
    - Use Artifact Registry image path for backend service

--- infra/terraform/google_cloud/development/cloudrun.tf
+++ infra/terraform/google_cloud/development/cloudrun.tf
@@ -18,8 +18,8 @@ resource "google_cloud_run_service" "backend" {
       containers {
         # TODO: こうすることで初回のterraform apply時にcloudbuildとの相互参照を回避できる
         # 2回目以降は正しいimageに変える
-        image = "gcr.io/cloudrun/hello"
-        # image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/backend"
+        # image = "gcr.io/cloudrun/hello"
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/${var.image_repo}/backend"
         ports {
           container_port = 50051
         }
